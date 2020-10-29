87a576d08a67be976c477e3df4a6a149644a0918
Allison MacQueen <allimacq@gmail.com>
2020-10-29 17:24:30 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,16 @@
 class Post
   
-  attr_accessor :author, :name
+  attr_accessor :author, :title
+  
+  @@all = [ ]
+  
+  def initialize(title)
+    @title = title
+    @@all < self
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
