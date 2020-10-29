e15c29a66c76f4a6aa3667f98f8279ebec4551e7
Allison MacQueen <allimacq@gmail.com>
2020-10-29 16:38:13 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+class Artist
+  attr_accessor :name, :song
+  
+  @@songs = [ ]
+  
+  def initialize(name)
+    @name = name
+    #@songs = [ ]
+  end
+
+  def new
+    @name
+    @@songs << self
+  end
+  
+  def songs
+    p Song.all
+    #Song.all.select {|song| song.artist == self}
+  end
+  
+  #def add_song(song)
+   # song.artist = self
+  #end
+
+end
